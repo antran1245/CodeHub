fb1ccbfca7749e9944425332870f619c9493e1cc
antran1245 <antran1245@gmail.com>
2022-06-29 11:14:32 -0700

    simple home push

--- src/main/java/com/java/project/controllers/HomeController.java
+++ src/main/java/com/java/project/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.java.project.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.java.project.models.Project;
 import com.java.project.services.ProjectService;
 
 @Controller
@@ -16,7 +19,7 @@ public class HomeController {
 //	======================= Display ==========================
 	@GetMapping("/")
 	public String home(Model model) {
-		List<Project> projects = 
+		List<Project> projects = projectService.allProjects(); 
 		return "home.jsp";
 	}
 	
